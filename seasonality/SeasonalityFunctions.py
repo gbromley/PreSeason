2a4340ec234554cce9442d778125fd9e3d8bc504
gbromley <bromlgab@gmail.com>
2023-08-25 09:01:33 -0600

    Added bombardi algorithms, but need fixing

--- seasonality/SeasonalityFunctions.py
+++ seasonality/SeasonalityFunctions.py
@@ -1,6 +1,31 @@
 
 import numpy as np
 
+
+def findFirst_numpy(a, b):
+    #TODO fill out function data
+    """
+    Summary:
+    --------
+    Calculates fourier series for data using:
+    
+    Input:
+    ------
+        tseries: Mean annual daily precipitation data. Should be of length 365.
+        n: Number of harmonics to calculate
+        time: Integer array of len(tseries)
+    
+    Output:
+    -------
+        a_n: Array of A_n fourier coefficients for Nth harmonics
+        b_n: Array of B_n fourier coefficients for Nth harmonics
+        var: Ratio of explained variance of nth harmonics
+    
+    """
+    inflection_array = np.lib.stride_tricks.sliding_window_view(a,len(b))
+    result = np.where(np.all(inflection_array == b, axis=1))
+    return result[0][0] if result else np.nan
+
 def fourier_coefficients(tseries, num_harm, time=None):
        
     """
@@ -67,7 +92,7 @@ def fourier_coefficients(tseries, num_harm, time=None):
        
     return a_n, b_n, var
 
-def harmonic_smoothing(tseries, num_harm, time=None):
+def smoothing_harmonic(tseries, num_harm, time=None):
     """
     Summary:
     --------
@@ -106,7 +131,7 @@ def harmonic_smoothing(tseries, num_harm, time=None):
     
     return smoothed     
 
-def first_harmonic_min(tseries):
+def min_first_harmonic(tseries):
     """
     Summary:
     --------
@@ -122,6 +147,133 @@ def first_harmonic_min(tseries):
         start_day: Calculation start day in julian days.
     
     """
-    harmonic = harmonic_smoothing(tseries,num_harm=1)
+    harmonic = smoothing_harmonic(tseries,num_harm=1)
     min_harmonic = np.argmin(harmonic)
     return min_harmonic
+
+def smooth_B17(tseries, num_passes=50):
+    """
+    Summary:
+    --------
+    Smooths a time series using a 1-2-1 filter. This filter is essentially
+    a moving average of window size 3.
+    
+    Input:
+    ------
+        tseries: Time series data, not limited to a particular length.
+        num_passes: Number of times to apply the smoothing to the time series.
+        Default is 50 times.
+    
+    Output:
+    -------
+        smoothie: Smoothed time series    
+    """
+    smoothie = tseries
+    temp = tseries
+    
+    for n in np.arange(0,num_passes):
+        temp[0] = 0.5*(smoothie[0]+smoothie[1])
+        temp[-1] = 0.5*(smoothie[-1]+smoothie[-2])
+        temp[1:-1] = 0.25*smoothie[0:-2] + 0.5*smoothie[1:-1]+0.25*smoothie[2:]
+        smoothie=temp
+    return smoothie
+
+#these all output the same as smooth-1-2-1 but might be faster on big arrays.
+def filter3(tseries,num_passes=1):
+    # Swap out convolve with the code below
+    # cumsum_vec = numpy.cumsum(numpy.insert(data, 0, 0)) 
+    # ma_vec = (cumsum_vec[window_width:] - cumsum_vec[:-window_width]) / window_width
+    output = tseries
+    inflection_array = output
+    for n in np.arange(0,num_passes):
+        inflection_array[0] = np.mean(output[0:2])
+        inflection_array[-1] = np.mean(output[-2:])
+        inflection_array[1:-1] = signal.convolve(output, np.array([1,1,1]), "valid")/3
+        output = inflection_array
+    return output
+ 
+def test_smooth(tseries,num_passes=1):
+    output = np.zeros(len(tseries))
+    inflection_array = output
+    coef= np.array([1,2,1])
+    output[0] = np.mean(tseries[0:2])
+    output[-1] = np.mean(tseries[-2:])
+    for i in np.arange(1,len(tseries) - 1,1):
+        
+        output[i] = np.sum(tseries[i-1:i+2] * coef / 4)
+    return output
+       
+def find_ddt_onset(tseries, window=5):
+    """
+    Summary:
+    --------
+    Returns the 1st derivative of an array
+    
+    Input:
+    ------
+        tseries: Time series data, not limited to a particular length.
+        
+    
+    Output:
+    -------
+        output: First derivative of input series.
+    
+    """
+    deriv = np.gradient(tseries)
+    
+    # Numpy function that returns the sign of each element
+    # -1 if negative, 0 if 0, 1 if positive
+    sign_array = np.sign(deriv)
+    
+    
+    
+    
+    # build up the inflection window
+    inflection_array = np.ones(window)
+    
+    # Finding the middle of the window, rounded down if odd.
+    split = int(np.floor(window/2))
+    
+    # Make first half -1
+    inflection_array[:split] = -1
+    
+    index = findFirst_numpy(sign_array, inflection_array)
+    
+    
+    
+    return index
+
+def find_ddt_demise(tseries, window=5):
+    """
+    Summary:
+    --------
+    Returns the 1st derivative of an array
+    
+    Input:
+    ------
+        tseries: Time series data, not limited to a particular length.
+        
+    
+    Output:
+    -------
+        output: First derivative of input series.
+    
+    """
+    deriv = np.gradient(tseries)
+    
+    # Numpy function that returns the sign of each element
+    # -1 if negative, 0 if 0, 1 if positive
+    sign_array = np.sign(deriv)
+    
+    # build up the inflection window
+    split = int(np.floor(window/2))
+    #mid = int(np.floor(window/2))
+    # build up the inflection window
+    inflection_array = np.ones(window)
+    inflection_array[split:] = -1
+    
+    index = findFirst_numpy(sign_array, inflection_array)
+    
+    
+    
+    return index
--- seasonality/onset_demise.py
+++ seasonality/onset_demise.py
@@ -1,6 +1,24 @@
 import numpy as np
 import xarray as xr
 import seasonality.seasonalityfunctions as sf
+#TODO Change the onset stuff to a class
+
+
+# TODO create decorator function for using apply_ufunc
+"""demise_LM01_test = xr.apply_ufunc(
+    demise_LM01,
+    anomalies,
+    anomalies.time,
+    start_wet2,
+    input_core_dims=[["time"],["time"],[]],
+    exclude_dims=set(["time"]),
+    output_core_dims=[["year"]],
+    vectorize=True,
+    dask = 'parallelized',
+    #output_dtypes = 'datetime64[D]',
+    #output_sizes={"data_jday": 71},
+)
+"""
 
 def B17_analysis_start(data):
     """
@@ -19,7 +37,7 @@ def B17_analysis_start(data):
     """
     
     output = xr.apply_ufunc(
-        sf.first_harmonic_min,
+        sf.min_first_harmonic,
         data,
         input_core_dims=[["dayofyear"]],
         exclude_dims=set(["dayofyear"]),
@@ -181,3 +199,167 @@ def demise_LM01(data, days, years, startWet):
         
         
     return demiseDOY
+
+def onset_B17(data, days, years, startWet):
+    
+    """
+    Summary:
+    --------
+    Calculates the start of a wet season using the Bombardi et al., 2017 methods. 
+    
+    Input:
+    ------
+        data: Daily precipitation anomalies.
+        days: Numpy array of day of year (1-365), no leap.
+        years: Numpy array of year for each item in days.
+        startWet: The doy for when to start the cumulative sum calcuation.
+    
+    Output:
+    -------
+        onsetDOY: Array of onset dates. Length is the same as the number of years on input data.
+
+    
+    """
+    # TODO move lines 206-250 to seperate class or function
+    if len(days) != len(years):
+        raise ValueError('Length of days and years must be the same.')
+        
+    # Want to make sure we get all the input years before any trimming
+    unique_years = np.unique(years)
+    
+    onsetDOY = np.empty((len(unique_years)))
+    onsetDOY[:] = np.nan
+    
+
+
+    # Day of year is missing integer 60 which is February 29th.
+    # Need to add zero because a tuple is returned from np.where
+    temp_start_index = np.where(days == startWet)[0]
+    
+    # double check we have enough data for last onset calculation
+    if len(days[temp_start_index[-1]:]) < 180:
+        
+        # trim off data we can't use
+        data_trimmed = data[temp_start_index[0]:temp_start_index[-1]]
+        days_trimmed = days[temp_start_index[0]:temp_start_index[-1]]
+        years_trimmed = years[temp_start_index[0]:temp_start_index[-1]]
+    else:
+        data_trimmed = data
+        days_trimmed = days
+        years_trimmed = years
+            
+    # Reindex start days with trimmed days  
+    # Need to add zero because a tuple is returned from np.where
+    start_day_index = np.where(days_trimmed == startWet)[0]
+    
+    ### looping through start dates ###
+    for start_day in start_day_index:
+        
+        # Make analysis period 180 days in length. 
+        #TODO #check that 180 days matters
+        analysis_begin = start_day
+        analysis_end = start_day + 180
+        
+        analysis_days = days_trimmed[analysis_begin:analysis_end]
+        analysis_years = years_trimmed[analysis_begin:analysis_end]
+        
+        cumsum_data = np.cumsum(data_trimmed[analysis_begin:analysis_end])
+        
+        smoothed_cs_data = sf.smooth_B17(cumsum_data)
+        
+        onset_index = sf.find_ddt_onset(smoothed_cs_data)
+        
+        
+        # this returns the index of the data not the day
+        onset_day = analysis_days[onset_index]
+        onset_year = analysis_years[onset_index]
+        
+        where_to_place = np.argwhere(unique_years == onset_year)[0][0]
+        onsetDOY[where_to_place] = onset_day
+        
+    return onsetDOY
+    
+def demise_B17(data, days, years, startWet):
+    
+    """
+    Summary:
+    --------
+    Calculates the start of a wet season using the Bombardi et al., 2017 methods. 
+    
+    Input:
+    ------
+        data: Daily precipitation anomalies.
+        days: Numpy array of day of year (1-365), no leap.
+        years: Numpy array of year for each item in days.
+        startWet: The doy for when to start the cumulative sum calcuation.
+    
+    Output:
+    -------
+        demiseDOY: Array of onset dates. Length is the same as the number of years on input data.
+
+    
+    """
+    # TODO move lines 206-250 to seperate class or function
+    
+    data = data[::-1]
+    days = days[::-1]
+    years = years[::-1] 
+    
+    if len(days) != len(years):
+        raise ValueError('Length of days and years must be the same.')
+        
+    # Want to make sure we get all the input years before any trimming
+    unique_years = np.unique(years)
+    
+    onsetDOY = np.empty((len(unique_years)))
+    onsetDOY[:] = np.nan
+    
+
+
+    # Day of year is missing integer 60 which is February 29th.
+    # Need to add zero because a tuple is returned from np.where
+    temp_start_index = np.where(days == startWet)[0]
+    
+    # double check we have enough data for last onset calculation
+    if len(days[temp_start_index[-1]:]) < 180:
+        
+        # trim off data we can't use
+        data_trimmed = data[temp_start_index[0]:temp_start_index[-1]]
+        days_trimmed = days[temp_start_index[0]:temp_start_index[-1]]
+        years_trimmed = years[temp_start_index[0]:temp_start_index[-1]]
+    else:
+        data_trimmed = data
+        days_trimmed = days
+        years_trimmed = years
+            
+    # Reindex start days with trimmed days  
+    # Need to add zero because a tuple is returned from np.where
+    start_day_index = np.where(days_trimmed == startWet)[0]
+    
+    ### looping through start dates ###
+    for start_day in start_day_index:
+        
+        # Make analysis period 180 days in length. 
+        #TODO #check that 180 days matters
+        analysis_begin = start_day
+        analysis_end = start_day + 180
+        
+        analysis_days = days_trimmed[analysis_begin:analysis_end]
+        analysis_years = years_trimmed[analysis_begin:analysis_end]
+        
+        cumsum_data = np.cumsum(data_trimmed[analysis_begin:analysis_end])
+        
+        smoothed_cs_data = sf.smooth_B17(cumsum_data)
+        
+        demise_index = sf.find_ddt_demise(smoothed_cs_data)
+        
+        
+        # this returns the index of the data not the day
+        onset_day = analysis_days[demise_index]
+        onset_year = analysis_years[demise_index]
+        
+        where_to_place = np.argwhere(unique_years == onset_year)[0][0]
+        onsetDOY[where_to_place] = onset_day
+        
+    return onsetDOY
+    
