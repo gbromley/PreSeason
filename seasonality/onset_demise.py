4bbb006035c68ea8667dad75a8293cd6e33f47fb
gbromley <bromlgab@gmail.com>
2023-08-28 15:52:48 -0600

    replaced lm01 with new cum anom code

--- seasonality/onset_demise.py
+++ seasonality/onset_demise.py
@@ -82,41 +82,13 @@ def onset_LM01(data, days, years, startWet):
 
     # Day of year is missing integer 60 which is February 29th.
     # Need to add zero because a tuple is returned from np.where
-    temp_start_index = np.where(days == startWet)[0]
+    start_index = np.where(days == startWet)[0]
     
-    # double check we have enough data for last onset calculation
-    if len(days[temp_start_index[-1]:]) < 180:
-        
-        # trim off data we can't use
-        data_trimmed = data[temp_start_index[0]:temp_start_index[-1]]
-        days_trimmed = days[temp_start_index[0]:temp_start_index[-1]]
-        years_trimmed = years[temp_start_index[0]:temp_start_index[-1]]
-    else:
-        data_trimmed = data
-        days_trimmed = days
-        years_trimmed = years
-            
-    # Reindex start days with trimmed days  
-    # Need to add zero because a tuple is returned from np.where
-    start_day_index = np.where(days_trimmed == startWet)[0]
     
     ### looping through start dates ###
-    for start_day in start_day_index:
+    for start_day in start_index:
         
-        # Make analysis period 180 days in length. 
-        #TODO #check that 180 days matters
-        analysis_begin = start_day
-        analysis_end = start_day + 180
-        
-        analysis_days = days_trimmed[analysis_begin:analysis_end]
-        analysis_years = years_trimmed[analysis_begin:analysis_end]
-        
-        cumsum_data = np.cumsum(data_trimmed[analysis_begin:analysis_end])
-        
-        # this returns the index of the data not the day
-        onset_index = np.argmin(cumsum_data)
-        onset_day = analysis_days[onset_index]
-        onset_year = analysis_years[onset_index]
+        onset_day, onset_year = sf.cumul_anom(data, days, years, start_day)
         
         where_to_place = np.argwhere(unique_years == onset_year)[0][0]
         onsetDOY[where_to_place] = onset_day
@@ -207,7 +179,7 @@ def test_onsetB17(data, days, years, startWet):
     outlier_locs = sf.check_outliers(output_onset_doy)
 """   
     
-    
+
 
 def onset_B17(data, days, years, startWet):
     
